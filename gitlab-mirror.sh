c4cdacf765e04778e07dbed0573361ffb0c632c0
gi8 <giottolino@gmail.com>
2020-09-03 22:13:23 +0200

    create bucket if not exists

--- gitlab-mirror.sh
+++ gitlab-mirror.sh
@@ -2,10 +2,14 @@
 
 set -o errexit
 
+bucketname=${BACKUP_BUCKET_NAME:-gitlab}
+
 mc config host add gitlab ${GITLAB_MINIO_URL} ${GITLAB_MINIO_ACCESSKEY} ${GITLAB_MINIO_SECRETKEY} --api S3v4
 
 mc config host add backup ${BACKUP_MINIO_URL} ${BACKUP_MINIO_ACCESSKEY} ${BACKUP_MINIO_SECRETKEY} --api S3v4
 
+mc mb --ignore-existing backup/gitlab
+
 mc rm --force --recursive --dangerous --older-than ${GITLAB_BACKUP_RETENTION} gitlab/gitlab-backups
 
-mc mirror gitlab/gitlab-backups backup/gitlab --remove 
+mc mirror gitlab/gitlab-backups backup/"${bucketname}" --remove
