1b4c80856ec9ad2d37b5338b3438566461114fee
gi8 <giottolino@gmail.com>
2025-06-06 13:20:22 +0200

    Revert "fix(mc): command to add sources"
    This reverts commit f2ae12bbf9272e45124b52cf7aca2552d4487263.

--- mirrio.sh
+++ mirrio.sh
@@ -7,44 +7,44 @@ RETENTION=${RETENTION:-30d0h0m}
 ADDITIONAL_PARAMETERS=${ADDITIONAL_PARAMETERS:---quiet}
 
 function pushgateway {
-  if [[ -n "${PUSHGATEWAY_URL}" ]]; then
-    cat <<-EOF | curl --retry 3 --max-time 5 --silent --show-error --data-binary @- "${PUSHGATEWAY_URL}/metrics/job/mirrio/instance/${JOB_NAME}" >/dev/null
+	if [[ -n "${PUSHGATEWAY_URL}" ]]; then
+		cat <<-EOF | curl --retry 3 --max-time 5 --silent --show-error --data-binary @- "${PUSHGATEWAY_URL}/metrics/job/mirrio/instance/${JOB_NAME}" >/dev/null
 			# TYPE mirrio_sync_failed gauge
 			# HELP mirrio_sync_failed Whether the last sync failed
 			mirrio_sync_failed $1
 		EOF
-  fi
+	fi
 }
 
 function check_env {
-  for var in "$@"; do
-    if [[ -z "${!var}" ]]; then
-      echo "${var} is not set!"
-      exit 1
-    fi
-  done
+	for var in "$@"; do
+		if [[ -z "${!var}" ]]; then
+			echo "${var} is not set!"
+			exit 1
+		fi
+	done
 }
 
 function notify {
-  pushgateway 1
-  echo "ERROR: Unable to sync!"
+	pushgateway 1
+	echo "ERROR: Unable to sync!"
 }
 
 check_env \
-  SOURCE_URL \
-  SOURCE_ACCESSKEY \
-  SOURCE_SECRETKEY \
-  DESTINATION_URL \
-  DESTINATION_ACCESSKEY \
-  DESTINATION_SECRETKEY \
-  SOURCE_BUCKET \
-  DESTINATION_BUCKET
+	SOURCE_URL \
+	SOURCE_ACCESSKEY \
+	SOURCE_SECRETKEY \
+	DESTINATION_URL \
+	DESTINATION_ACCESSKEY \
+	DESTINATION_SECRETKEY \
+	SOURCE_BUCKET \
+	DESTINATION_BUCKET
 
 trap notify ERR
 
-mc admin bucket remote add source "${SOURCE_URL}" "${SOURCE_ACCESSKEY}" "${SOURCE_SECRETKEY}" --api S3v4
+mc config host add source "${SOURCE_URL}" "${SOURCE_ACCESSKEY}" "${SOURCE_SECRETKEY}" --api S3v4
 
-mc admin bucket remote add destination "${DESTINATION_URL}" "${DESTINATION_ACCESSKEY}" "${DESTINATION_SECRETKEY}" --api S3v4
+mc config host add destination "${DESTINATION_URL}" "${DESTINATION_ACCESSKEY}" "${DESTINATION_SECRETKEY}" --api S3v4
 
 mc ls destination | grep -q "${DESTINATION_BUCKET}" || mc mb "destination/${DESTINATION_BUCKET}"
 
